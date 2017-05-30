4ef77afc728c358e74fcbfacf20a11d9dc092cda
Dmitry V. Levin <ldv@altlinux.org>
2017-05-30 15:37:59 +0000

    get_arg_page: limit argv+env strings size for suid/sgid binaries
     Conflicts:
    	fs/exec.c
    
     Author:    Dmitry V. Levin <ldv@altlinux.org>
     Date:      Tue May 30 15:37:59 2017 +0000

--- fs/exec.c
+++ fs/exec.c
@@ -218,10 +218,71 @@ static struct page *get_arg_page(struct linux_binprm *bprm, unsigned long pos,
 	if (ret <= 0)
 		return NULL;
 
-	if (write)
+	if (write) {
+		unsigned long size = bprm->vma->vm_end - bprm->vma->vm_start;
+		unsigned long ptr_size, limit;
+
+		/*
+		 * Since the stack will hold pointers to the strings, we
+		 * must account for them as well.
+		 *
+		 * The size calculation is the entire vma while each arg page is
+		 * built, so each time we get here it's calculating how far it
+		 * is currently (rather than each call being just the newly
+		 * added size from the arg page).  As a result, we need to
+		 * always add the entire size of the pointers, so that on the
+		 * last call to get_arg_page() we'll actually have the entire
+		 * correct size.
+		 */
+		ptr_size = (bprm->argc + bprm->envc) * sizeof(void *);
+		if (ptr_size > ULONG_MAX - size)
+			goto fail;
+		size += ptr_size;
+
+		acct_arg_size(bprm, size / PAGE_SIZE);
+
+		/*
+		 * We've historically supported up to 32 pages (ARG_MAX)
+		 * of argument strings even with small stacks
+		 */
+		if (size <= ARG_MAX)
+			return page;
+
+		/*
+		 * Limit to 1/4 of the max stack size or 3/4 of _STK_LIM
+		 * (whichever is smaller) for the argv+env strings.
+		 * This ensures that:
+		 *  - the remaining binfmt code will not run out of stack space,
+		 *  - the program will have a reasonable amount of stack left
+		 *    to work from.
+		 */
+		limit = _STK_LIM / 4 * 3;
+		limit = min(limit, rlimit(RLIMIT_STACK) / 4);
+		if (size > limit)
+			goto fail;
+
+		/*
+		 * In case of suid/sgid binaries, limit the stack size for
+		 * the argv+env strings to 512KB, preventing from clashing
+		 * the stack with other memory allocations through megabytes
+		 * of command-line arguments and environment variables.
+		 * Unfortunately, AT_SECURE binaries other than suid/sgid ones
+		 * are not covered by this limit, but security_bprm_secureexec
+		 * is not available at this point yet, so this is all we can do.
+		 */
+		if (size > (512UL << 10) &&
+		    (!uid_eq(bprm->cred->euid, current_euid()) ||
+		     !gid_eq(bprm->cred->egid, current_egid()))) {
+			goto fail;
+		}
 		acct_arg_size(bprm, vma_pages(bprm->vma));
+	}
 
 	return page;
+
+fail:
+       put_page(page);
+       return NULL;
 }
 
 static void put_arg_page(struct page *page)
