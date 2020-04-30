c385337be6da1926a6bf7fef4d264508841e805c
Alexey Sheplyakov <asheplyakov@altlinux.org>
2020-04-30 15:46:43 +0400

    Baikal-M: video unit driver
    Based on code from SDK-M-4.3 with the following improvements:
    
    * Fixed autoloading when compiled as a module
    * Avoid concurrent calls to BAIKAL_SMC_VDU_UPDATE_HDMI
    * Added missing drm_crtc_vblank_{on,off}
    * Moved enable/disable_vblank to crtc_funcs, so atomic_flip users
      (Wayland compositors) work as expected [1]
    * Removed empty atomic_check
    * Use `drm_fb_cma_get_gem_addr` instead of reimplementing it
    * Use `drm_gem_cma_dumb_create` instead of reimplementing it
    
    [1] atomic_flush callback uses drm_crtc_vblank_get, and normally arms
    the drm_pending_vblank_event. However if drm_crtc_vblank_get indicates
    an error, atomic_flush tries to send vblank_event immediately. Since kernel
    v5.7-rc1 drm_crtc_vblank get relies on enable/disable_vblank callbacks in
    crtc_funcs. If those callbacks are not defined, drm_crtc_vblank_get always
    returns -EINVAL. Thus (baikal_vdu) atomic_flush always sends the pending
    vblank event (synchronously). This causes "event storm" when the userspace
    handles atomic flip:
    
        at ../libweston/compositor.c:2957
        stack_end=<optimized out>) at ../csu/libc-start.c:308
    
    So attempt to repaint screen causes yet another repaint event, which causes
    another repaint, etc.

---
+++ drivers/gpu/drm/baikal/baikal_vdu_drm.h
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2019-2020 Baikal Electronics JSC
+ *
+ * Author: Pavel Parkhomenko <Pavel.Parkhomenko@baikalelectronics.ru>
+ *
+ * Parts of this file were based on sources as follows:
+ *
+ * Copyright (c) 2006-2008 Intel Corporation
+ * Copyright (c) 2007 Dave Airlie <airlied@linux.ie>
+ * Copyright (C) 2011 Texas Instruments
+ * (C) COPYRIGHT 2012-2013 ARM Limited. All rights reserved.
+ *
+ * This program is free software and is provided to you under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation, and any use by you of this program is subject to the terms of
+ * such GNU licence.
+ *
+ */
+
+#ifndef __BAIKAL_VDU_DRM_H__
+#define __BAIKAL_VDU_DRM_H__
+
+#include <drm/drm_gem.h>
+#include <drm/drm_simple_kms_helper.h>
+#include <linux/workqueue.h>
+
+struct baikal_vdu_private {
+	struct drm_device *drm;
+
+	struct drm_connector connector;
+	struct drm_crtc crtc;
+	struct drm_encoder encoder;
+	struct drm_panel *panel;
+	struct drm_bridge *bridge;
+	struct drm_plane primary;
+
+	void *regs;
+	struct clk *clk;
+	spinlock_t lock;
+	u32 counters[20];
+	int mode_fixup;
+
+	u32 fb_addr;
+	u32 fb_end;
+
+	struct delayed_work update_work;
+};
+
+/* CRTC Functions */
+int baikal_vdu_crtc_create(struct drm_device *dev);
+irqreturn_t baikal_vdu_irq(int irq, void *data);
+
+int baikal_vdu_primary_plane_init(struct drm_device *dev);
+
+/* Connector Functions */
+int baikal_vdu_connector_create(struct drm_device *dev);
+
+/* Encoder Functions */
+int baikal_vdu_encoder_init(struct drm_device *dev);
+
+void baikal_vdu_debugfs_init(struct drm_minor *minor);
+
+/* Worker functions */
+void baikal_vdu_update_work(struct work_struct *work);
+
+#endif /* __BAIKAL_VDU_DRM_H__ */
