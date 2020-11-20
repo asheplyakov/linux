c0e43bb9b3cd5972ebca0883608baaa7cc8d55d4
Alexey Sheplyakov <asheplyakov@altlinux.org>
2020-11-20 14:11:53 +0400

    Qt Creator project files and deployment scripts

---
+++ update-qtcreator-files.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+list_sources () {
+	local arches="$@"
+	for arch in $arches; do
+		git ls-files "arch/$arch/**.[chS]" | grep -v -E 'boot/dts/'
+	done 
+	git ls-files '*.[chS]' | grep -v -E '^(arch)|(Documentation)|(tools)|(virt)'
+}
+
+list_sources arm arm64 | sort -u
+
