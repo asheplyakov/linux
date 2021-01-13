bittencourtjulival/eclipse_kernel_xiaomi_stone

PCI: dwc: Export APIs to support .remove() implementation
Export all configuration space access APIs and also other APIs to
support host controller drivers of dwc core based implementations while
adding support for .remove() hook to build their respective drivers as
modules.

Signed-off-by: Vidya Sagar <vidyas@nvidia.com>
Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
Acked-by: Gustavo Pimentel <gustavo.pimentel@synopsys.com>

drivers/pci/controller/dwc/pcie-designware.c
@@ -34,6 +34,7 @@ int dw_pcie_read(void __iomem *addr, int size, u32 *val)
 
 	return PCIBIOS_SUCCESSFUL;
 }
+EXPORT_SYMBOL_GPL(dw_pcie_read);
 
 int dw_pcie_write(void __iomem *addr, int size, u32 val)
 {
@@ -51,6 +52,7 @@ int dw_pcie_write(void __iomem *addr, int size, u32 val)
 
 	return PCIBIOS_SUCCESSFUL;
 }
+EXPORT_SYMBOL_GPL(dw_pcie_write);
 
 u32 dw_pcie_read_dbi(struct dw_pcie *pci, u32 reg, size_t size)
 {
@@ -66,6 +68,7 @@ u32 dw_pcie_read_dbi(struct dw_pcie *pci, u32 reg, size_t size)
 
 	return val;
 }
+EXPORT_SYMBOL_GPL(dw_pcie_read_dbi);
 
 void dw_pcie_write_dbi(struct dw_pcie *pci, u32 reg, size_t size, u32 val)
 {
@@ -80,6 +83,7 @@ void dw_pcie_write_dbi(struct dw_pcie *pci, u32 reg, size_t size, u32 val)
 	if (ret)
 		dev_err(pci->dev, "Write DBI address failed\n");
 }
+EXPORT_SYMBOL_GPL(dw_pcie_write_dbi);
 
 u32 dw_pcie_read_dbi2(struct dw_pcie *pci, u32 reg, size_t size)
 {

asheplyakov/linux

(cherry picked from commit ca98329d3b58ef636b8f455b0b1b6ac9c89738b2)
Alexey Sheplyakov: adapted for Linux 4.9.y
(cherry picked from commit 6387b4dd9e1c1796cbbe6d30967c32256e2f6b17)

drivers/pci/host/pcie-designware.c
@@ -140,6 +140,7 @@ int dw_pcie_cfg_write(void __iomem *addr, int size, u32 val)
 
 	return PCIBIOS_SUCCESSFUL;
 }
+EXPORT_SYMBOL_GPL(dw_pcie_cfg_write);
 
 u32 dw_pcie_readl_rc(struct pcie_port *pp, u32 reg)
 {
@@ -148,6 +149,7 @@ u32 dw_pcie_readl_rc(struct pcie_port *pp, u32 reg)
 
 	return readl(pp->dbi_base + reg);
 }
+EXPORT_SYMBOL_GPL(dw_pcie_readl_rc);
 
 void dw_pcie_writel_rc(struct pcie_port *pp, u32 reg, u32 val)
 {
@@ -156,6 +158,7 @@ void dw_pcie_writel_rc(struct pcie_port *pp, u32 reg, u32 val)
 	else
 		writel(val, pp->dbi_base + reg);
 }
+EXPORT_SYMBOL_GPL(dw_pcie_writel_rc);
 
 static u32 dw_pcie_readl_unroll(struct pcie_port *pp, u32 index, u32 reg)
 {
@@ -295,6 +298,7 @@ void dw_pcie_msi_init(struct pcie_port *pp)
 	dw_pcie_wr_own_conf(pp, PCIE_MSI_ADDR_HI, 4,
 			    (u32)(msi_target >> 32 & 0xffffffff));
 }
+EXPORT_SYMBOL_GPL(dw_pcie_msi_init);
 
 static void dw_pcie_msi_clear_irq(struct pcie_port *pp, int irq)
 {
@@ -477,6 +481,7 @@ int dw_pcie_wait_for_link(struct pcie_port *pp)
 
 	return -ETIMEDOUT;
 }
+EXPORT_SYMBOL_GPL(dw_pcie_wait_for_link);
 
 int dw_pcie_link_up(struct pcie_port *pp)
 {
@@ -675,6 +680,7 @@ error:
 	pci_free_resource_list(&res);
 	return ret;
 }
+EXPORT_SYMBOL_GPL(dw_pcie_host_init);
 
 static int dw_pcie_rd_other_conf(struct pcie_port *pp, struct pci_bus *bus,
 		u32 devfn, int where, int size, u32 *val)
@@ -900,3 +906,4 @@ void dw_pcie_setup_rc(struct pcie_port *pp)
 	val |= PORT_LOGIC_SPEED_CHANGE;
 	dw_pcie_wr_own_conf(pp, PCIE_LINK_WIDTH_SPEED_CONTROL, 4, val);
 }
+EXPORT_SYMBOL_GPL(dw_pcie_setup_rc);