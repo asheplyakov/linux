d7a450ca1e1bc8023cf4f99ac70579701d8f1198
Alexey Sheplyakov <asheplyakov@basealt.ru>
2022-05-23 19:32:24 +0400

    drm: dw-hdmi-ahb-audio: support Baikal-M SoC
    Signed-off-by: Alexey Sheplyakov <asheplyakov@basealt.ru>
    X-feature-Baikal-M

--- drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
+++ drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
@@ -3442,6 +3442,12 @@ struct dw_hdmi *dw_hdmi_probe(struct platform_device *pdev,
 		audio.irq = irq;
 		audio.hdmi = hdmi;
 		audio.get_eld = hdmi_audio_get_eld;
+		audio.regshift = 0;
+		if (of_device_is_compatible(np, "baikal,hdmi")) {
+			audio.regshift = 2;
+			dev_info(dev, "setting audio.reg_offset=%d for BE-M1000 SoC\n",
+				 audio.regshift);
+		}
 		hdmi->enable_audio = dw_hdmi_ahb_audio_enable;
 		hdmi->disable_audio = dw_hdmi_ahb_audio_disable;
 
